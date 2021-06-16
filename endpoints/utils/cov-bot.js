760eaaa4bf8c173e753a6c16715274c7c1980c92
AntiPrim3 <amit.rockinad.das@gmail.com>
2021-06-17 02:27:07 +0530

    changed reply format

--- endpoints/utils/cov-bot.js
+++ endpoints/utils/cov-bot.js
@@ -19,10 +19,36 @@ covBot.command("vaccine", async (ctx) => {
   if (!vaccineData)
     await ctx.replyWithHTML(`<strong>${state}</strong>: No Data found!`);
   else {
-    let dataHtml = "";
-    for (dataPoint in vaccineData) {
-      dataHtml += `<strong>${dataPoint}</strong>. ${vaccineData[dataPoint]}\n`;
-    }
+    let dataHtml = `<strong>${
+      vaccineData["State"] || state
+    } data as Updated On: ${vaccineData["UpdatedOn"]}</strong>\n\n`;
+
+    dataHtml += `<strong>Males Vaccinated</strong>: ${
+      vaccineData["Male(IndividualsVaccinated)"] || 0
+    }\n`;
+    dataHtml += `<strong>Females Vaccinated</strong>: ${
+      vaccineData["Female(IndividualsVaccinated)"] || 0
+    }\n`;
+    dataHtml += `<strong>Transgenders Vaccinated</strong>: ${
+      vaccineData["Transgender(IndividualsVaccinated)"] || 0
+    }\n`;
+    dataHtml += `<strong>Total</strong>: ${
+      vaccineData["TotalIndividualsVaccinated"] || 0
+    }\n`;
+
+    dataHtml += `<strong>18+ Vaccinated</strong>: ${
+      vaccineData["18-45years(Age)"] || 0
+    }\n`;
+    dataHtml += `<strong>45+ Vaccinated</strong>: ${
+      vaccineData["45-60years(Age)"] || 0
+    }\n`;
+    dataHtml += `<strong>60+ Vaccinated</strong>: ${
+      vaccineData["60+years(Age)"] || 0
+    }\n`;
+    dataHtml += `<strong>Total</strong>: ${
+      vaccineData["TotalIndividualsVaccinated"] || 0
+    }\n`;
+
     await ctx.replyWithHTML(dataHtml);
   }
 });
